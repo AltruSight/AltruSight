ef0e5dd08f30feb08dd20779cd0ba52709f44929
AndyHuh98 <andrewhe@uark.edu>
2021-02-05 12:41:34 -0600

    added basic user auth to login form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,18 +11,33 @@ export class LoginComponent implements OnInit {
   hide = true;
   loginError = false;
 
-  testEmail = ["rtoche@uark.edu"]
-  testPassword = ["legomyego"]
+  testEmail = ['rtoche@uark.edu'];
+  testPassword = ['legomyego'];
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth) { }
 
   ngOnInit(): void {
   }
 
-  login(userEmail: string, userPassword: string):void {
+  login(userEmail: string, userPassword: string): void {
     console.log(userEmail);
     console.log(userPassword);
-    this.loginError = true;
+
+    // Once registration form is complete, similar method is used there except with createUserWithEmailAndPassword (something like that)
+    this.auth.signInWithEmailAndPassword(userEmail, userPassword)
+      .then(() => {
+        // ANDREW - Adding behavior to user authentication
+        // generally we could either have a pop up for this information
+        console.log('Logged in successfully');
+        // or, redirect to home page, or do both (pop up then redirect after exiting popup)
+      })
+      .catch((error) => {
+        // RAFA - Handling validation
+        // in this case, just send the error message case to validation and use a series of switch statements
+        // the types of error message cases should be listed on the firebase auth documentation
+        console.error(error);
+        this.loginError = true;
+      });
   }
 
 
