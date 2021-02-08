0e45235f6f9dd1a7be365ca541e0258035932dce
AndyHuh98 <andrewhe@uark.edu>
2021-02-08 16:21:38 -0600

    added authentication to register form

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,15 +12,32 @@ export class RegisterComponent implements OnInit {
   hide = true;
   registerError = false;
 
-  constructor() { }
+  constructor(public auth: AngularFireAuth, public router: Router) { }
 
   ngOnInit(): void {
   }
 
-  register(userEmail: string, userPassword: string, userConfirmPassword: string):void {
-    console.log(userEmail);
-    console.log(userPassword);
-    console.log(userConfirmPassword);
+  // fake validation of confirm passwords
+  userPasswordIsValid(): boolean {
+    return true;
+  }
+
+  register(userEmail: string, userPassword: string, userConfirmPassword: string): void {
+    if (userPassword === userConfirmPassword && this.userPasswordIsValid()) {
+      // RAFA: Add validation here (if passswords don't match, throw error)
+      this.auth.createUserWithEmailAndPassword(userEmail, userPassword)
+      .then(() => {
+        console.log(userEmail);
+        console.log(userPassword);
+        console.log(userConfirmPassword);
+        this.router.navigateByUrl('/');
+      })
+      .catch((error) => {
+        // Use error in validation
+        console.error(error);
+      });
+    }
+
     this.registerError = true;
   }
 
