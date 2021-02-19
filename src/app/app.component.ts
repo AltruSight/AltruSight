2cc0c9f55871277bf2e0fa1540ef12689aa303a7
AndyHuh98 <andrewhe@uark.edu>
2021-02-19 12:57:55 -0600

    added placeholder login / register buttons to nav bar for testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'altrusight';
+
+  constructor(public router: Router) {}
+
+  navigateTo(path: string): void {
+    this.router.navigateByUrl(`/${path}`);
+  }
 }
